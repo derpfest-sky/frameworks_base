954ec07f54e109a7798256294f07cfb41d7c7ae4
SagarMakhar <sagarmakhar@gmail.com>
2021-12-21 15:34:11 +0000

    SystemUI: Bring back UDFPS animation
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Co-authored-by: daniml3 <danimoral1001@gmail.com>
    Co-authored-by: DennySPb <dennyspb@gmail.com>
    Co-authored-by: kondors1995 <normandija1945@gmail.com>
    Co-authored-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Co-authored-by: Hikari-no-Tenshi <Hikari-no-Tenshi@users.noreply.github.com>
    Co-authored-by: TheScarastic <warabhishek@gmail.com>
    Co-authored-by: nullxception <nullxception@users.noreply.github.com>
    Co-authored-by: 00day0 <00day0@users.noreply.github.com>
    
    [Dhina17]
    - Drop Udfps icon customization
    - Move to Settings.Secure.
    - Use ContentObserver instead of TunerService
    
    Co-authored-by: Dhina17 <dhinalogu@gmail.com>
    Change-Id: I0f0fcde998ad3d80f22f76f7136f141331ca6c7c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12961,6 +12961,16 @@ public final class Settings {
         public static final String PULSE_VERTICAL_MIRROR = "pulse_vertical_mirror";
 
         /**
+         * @hide
+         */
+        public static final String UDFPS_ANIM = "udfps_anim";
+
+        /**
+         * @hide
+         */
+        public static final String UDFPS_ANIM_STYLE = "udfps_anim_style";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
